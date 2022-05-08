d8ca175a512b024d99fa3230e19684b352060204
jony <jony@Andress-Mac-mini.local>
2022-05-08 12:46:00 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8145a6871ec767a0c1af847a0277dc9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '093dd796-fe5c-4090-9cbb-2157f43a4fa3'
+};
